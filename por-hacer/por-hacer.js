f99931695449c36f70ee402e5a8ded67ec0940da
Borja <borja.iranzo@gmail.com>
2018-12-07 21:59:49 +0100

    Primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('./db/data.json', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+
+}
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer.splice(index);
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+    //console.log(listadoPorHacer);
+}
+
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    console.log(porHacer);
+    return porHacer;
+}
+
+module.exports = {
+    crear,
+    guardarDB,
+    getListado,
+    actualizar,
+    borrar
+};
